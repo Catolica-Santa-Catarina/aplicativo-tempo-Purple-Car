484b2de14a4fd3e612ac9541212e248b9ecbae29
Eduardo H <eduardodamaiahaak@gmail.com>
2023-09-28 21:59:01 -0300

    Aula 28-09-2023

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,10 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
+import '../models/location.dart';
+import '../services/location_service.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -8,6 +14,33 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  Future<void> getLocation() async {
+    LocationService location = LocationService();
+    Location locations = await location.getCurrentLocation();
+
+    log("Altitude: ${locations.latitude} - Longitude: ${locations.longitude}");
+  }
+
+  Future<void> getWeatherData() async {
+    var url = Uri.parse(
+        "https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22");
+    var response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      var data = response.body;
+      log(data);
+    } else {
+      log(response.statusCode.toString());
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+    getWeatherData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +48,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
         child: ElevatedButton(
           onPressed: () {
             // obtém a localização atual
+            getLocation();
           },
           child: const Text('Obter Localização'),
         ),
